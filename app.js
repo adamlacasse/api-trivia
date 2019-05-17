9362d06750f7b15abde1b3308b9a8feeef64ccc3
Adam LaCasse <adamlacasse@outlook.com>
2019-05-16 21:30:37 -0400

    wip

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ $.ajax({
 }).then(function (response) {
     // console.log(response.results)
     questions = response.results
-    // console.log(questions[currentQuestion])
-    $("#question").text(questions[currentQuestion].question)
+    console.log(questions[currentQuestion])
+    $("#question").html(questions[currentQuestion].question)
+    questions[currentQuestion].incorrect_answers.forEach(e => {
+        $("#answers").append(`<button>${e}</button>`)
+    });
+    $("#answers").append(`<button>${questions[currentQuestion].correct_answer}</button>`)
 });
 
